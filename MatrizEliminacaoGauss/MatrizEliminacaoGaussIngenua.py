2bb6533694a4a75020127bd861bc8357fac3c794
AntunesLeonardo <antunes.leonardo.a@gmail.com>
2021-10-04 11:12:56 -0300

    Eliminação Gauss comcluido

--- MatrizEliminacaoGauss/MatrizEliminacaoGaussIngenua.py
+++ MatrizEliminacaoGauss/MatrizEliminacaoGaussIngenua.py
@@ -1,20 +1,22 @@
 """
-Solução de Sistema de Equações:
+Algorithm for finding x value where the function f(x) crosses the X axis.
 
--- Eliminação de Gauss Ingênua --
-
-
-@author: Leonardo A. Antunes
+The input should be the equation system in form of matrix.
+The output is the X solutions for the equations.
+Author: AntunesLeonardo
 """
+# Library import ------------------------------------------
 
 import numpy as np
 
-# Matriz de Entrada ---------------------------------------------
-matriz = np.array([[0.006, 2, -16, -7, 14], [-7, 0.01, -16, 14, -3], [-0.02, -8, -9, 15, 17], [2, -7, 9, 5, -12]], dtype='f8')
+# Variable Input ------------------------------------------
+
+matriz = np.array([[0.006, 2, -16, -7, 14], [-7, 0.01, -16, 14, -3], [-0.02, -8, -9, 15, 17], [2, -7, 9, 5, -12]], dtype=float)
 # [[0.006, 2, -16, -7, 14], [-7, 0.01, -16, 14, -3], [-0.02, -8, -9, 15, 17], [2, -7, 9, 5, -12]][[1, 5, 1, -8], [2, 3, 10, 6], [10, 2, 1, 7]]
 
-# Funções que Modificam a Matriz
-def anulaElemeto(mtrx, lin, col, maX):
+# Functions -----------------------------------------------
+
+def anulaElemeto(mtrx, lin, col, maX):                     # Tunr element to zero, and rearrenge the line
     da = mtrx[lin, col] / mtrx[col, col]
     i = 0
     while i < maX + 1:
@@ -22,7 +24,7 @@ def anulaElemeto(mtrx, lin, col, maX):
         i += 1
     return mtrx
 
-def triangular(mtrx, lins, cols):
+def triangular(mtrx, lins, cols):                          # Makes matrix triangular shaped
     # O j vai antes, processo coluna-por-coluna
     j = 0
     while j < cols:
@@ -32,22 +34,15 @@ def triangular(mtrx, lins, cols):
                 mtrx = anulaElemeto(mtrx, i, j, lins)
             i += 1
         j += 1
-    #print(mtrx)
     return mtrx
 
-# Função Principal
-def eGaussIng(mtrx):
-    # Definição de Variáveis
-    lins, cols = mtrx.shape
+def MatrizEGaussIng(mtrx):                                       # Main function
+    lins, cols = mtrx.shape                                # Get matrix shape
+    x = np.zeros((lins), dtype='f4')                       # Create results matrix
     
-    # Preenchimento da Lista de Respostas (x)
-    x = np.zeros((lins), dtype='f4')
+    mtrx = triangular(mtrx, lins, cols)                    # Modifications of the input matrix
     
-    # Modificação da Matriz
-    mtrx = triangular(mtrx, lins, cols)
-    # print(mtrx)
-    
-    # Cálculo dos Resultados
+    # Result calculus
     i = 1
     while i < cols:
          j = 1
@@ -60,4 +55,11 @@ def eGaussIng(mtrx):
          i += 1
     return x
 
-print('Eliminção de Gauss Ingênua: \n', 'X:', eGaussIng(matriz))
+# Results display =========================================
+
+result = MatrizEGaussIng(matriz)
+print('\nMatriz de entrada:\n', matriz)
+print('\nResultados (Gauss Ingênua):\n')
+for i in range(0, len(result)):
+    print('X', i+1, '=', result[i])
+print('')                                                  # Empty line
